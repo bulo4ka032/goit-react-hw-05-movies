87737f6a60102332fa04e807128896d0a2142eda
bulo4ka032 <122801694+bulo4ka032@users.noreply.github.com>
2023-05-01 08:06:49 +0000

    Deploying to gh-pages from @ bulo4ka032/goit-react-hw-05-movies@8db37962b4665a7e64c72927799c78f9f919e2ed 🚀

---
+++ static/js/471.4d40a200.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[471],{9471:function(e,n,t){t.r(n),t.d(n,{default:function(){return f}});var r,i,s,a=t(168),c=t(7402),o=c.Z.img(r||(r=(0,a.Z)(["\n  width: 500px;\n"]))),u=c.Z.p(i||(i=(0,a.Z)(["\n  font-size: 36px;\n  font-weight: 300;\n"]))),h=c.Z.div(s||(s=(0,a.Z)(["\n  text-align: center;\n"]))),l=t(184),f=function(e){var n=e.message,t=e.img;return(0,l.jsxs)(h,{children:[(0,l.jsx)(o,{src:t,alt:"error"}),(0,l.jsx)(u,{children:n})]})}}}]);
+//# sourceMappingURL=471.4d40a200.chunk.js.map
